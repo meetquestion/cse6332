36994f4ccd61d5d6201cc928ba1eb73623ba4edd
21xiu <823793426@qq.com>
2023-04-01 09:56:24 +0800

    delete

--- src/main/java/com/bupt/uta/entity/Supplier.java
+++
@@ -1,9 +0,0 @@
-package com.bupt.uta.entity;
-
-public class Supplier {
-    private int id;
-    private String name;
-    private String email;
-    private String password;
-    private String phone;
-}
