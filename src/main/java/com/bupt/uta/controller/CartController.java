3de8017b505b1c00a80dc2af33891ff7bfeb6e34
jingxp <1484225519@qq.com>
2023-04-01 10:22:00 +0800

    param

--- src/main/java/com/bupt/uta/controller/CartController.java
+++ src/main/java/com/bupt/uta/controller/CartController.java
@@ -5,26 +5,13 @@ import com.bupt.uta.common.R;
 import com.bupt.uta.entity.Cart;
 import com.bupt.uta.entity.CartVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-/**
- * <p></p>
- * <p>
- * <PRE>
- * <BR>    修改记录
- * <BR>-----------------------------------------------
- * <BR>    修改日期         修改人          修改内容
- * </PRE>
- *
- * @author jingxp
- * @version 1.0
- * @date Created in 2023年03月31日 16:16
- * @since 1.0
- */
 @RestController
 @RequestMapping("/api")
 public class CartController {
@@ -32,8 +19,8 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
-    @RequestMapping(value = "/getCartVoList",method = RequestMethod.GET)
-    public R<List<CartVo>> getCartVoList(Long customerId) {
+    @RequestMapping(value = "/getCartVoList/{customerId}",method = RequestMethod.GET)
+    public R<List<CartVo>> getCartVoList(@PathVariable Long customerId) {
         if(customerId==null){
             return R.error("error");
         }
