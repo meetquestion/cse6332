743083a482e3b0238d25b486d5553b868ec178fe
jingxp <1484225519@qq.com>
2023-04-23 21:19:23 +0800

    update communicate

--- src/main/java/com/bupt/uta/controller/CommunicateController.java
+++ src/main/java/com/bupt/uta/controller/CommunicateController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Slf4j
 @RestController
 @RequestMapping("/api")
@@ -26,8 +29,8 @@ public class CommunicateController {
         }
         try{
             LambdaQueryWrapper<Communicate> wrapper = new LambdaQueryWrapper<>();
-            wrapper.eq(Communicate::getAdminId,communicate.getAdminId())
-                    .eq(Communicate::getCustomerId, communicate.getCustomerId());
+            wrapper.eq(Communicate::getReceiverId,communicate.getReceiverId())
+                    .eq(Communicate::getSenderId, communicate.getSenderId());
             Communicate one = communicateService.getOne(wrapper);
             return R.success(one);
         }catch (Exception e){
@@ -35,5 +38,24 @@ public class CommunicateController {
             return R.error("error");
         }
 
+    }
+
+    @RequestMapping(value = "/communicate/post",method = RequestMethod.POST)
+    public R<Communicate> save(@RequestBody Communicate communicate){
+        if(communicate == null){
+            return R.error("error");
+        }
+        try{
+            Date currentTime = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String dateString = formatter.format(currentTime);
+            Date date = formatter.parse(dateString);
+            communicate.setCreateTime(date);
+            communicateService.save(communicate);
+        }catch (Exception e){
+            log.error(e.getMessage());
+            R.error("error,e");
+        }
+        return R.success(communicate);
     }
 }
