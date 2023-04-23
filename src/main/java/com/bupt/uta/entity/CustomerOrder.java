2511d7c217d2d61e6a688af152c32fa406423378
jingxp <1484225519@qq.com>
2023-04-23 17:52:19 +0800

    order

--- src/main/java/com/bupt/uta/entity/CustomerOrder.java
+++ src/main/java/com/bupt/uta/entity/CustomerOrder.java
@@ -1,5 +1,7 @@
 package com.bupt.uta.entity;
 
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +17,12 @@ public class CustomerOrder {
     private Long productId;
     // 0:pay 1: return
     private int status;
-
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
     private Date modifyTime;
+    //人
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    //出
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 }
