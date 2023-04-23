78f36bab994e1eb143664a41b2df334fbfe8f196
jingxp <1484225519@qq.com>
2023-04-23 11:38:40 +0800

    cart 添加删除

--- src/main/java/com/bupt/uta/mapper/CartMapper.java
+++ src/main/java/com/bupt/uta/mapper/CartMapper.java
@@ -9,24 +9,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-/**
- * <p></p>
- * <p>
- * <PRE>
- * <BR>    修改记录
- * <BR>-----------------------------------------------
- * <BR>    修改日期         修改人          修改内容
- * </PRE>
- *
- * @author jingxp
- * @version 1.0
- * @date Created in 2023年03月31日 15:53
- * @since 1.0
- */
+
 @Mapper
 @Repository
 public interface CartMapper extends BaseMapper<Cart> {
     @Select("select cart.id as cartId, customer_id as customerId, product_id as productId, status, pro.name as productName, coding, price,category, pic,num " +
-            "from cart left join product pro on product_id = pro.id where customer_id = #{customerId}")
+            "from cart left join product pro on product_id = pro.id where customer_id = #{customerId} and status = 1")
     List<CartVo> getCartList(Cart cart);
+
 }
